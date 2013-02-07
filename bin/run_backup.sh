6e34f1cb5d5a7b73e7dd48307229ccce28e6df35
seikath <seikath@gmail.com>
2013-02-07 12:05:01 +0100

    fixed the backup to run from the data node and added its config config

--- bin/run_backup.sh
+++ bin/run_backup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #run_backup.sh
 ### Properties ####
+# 2013-02-07.12.01.57
 
-FILE_LOG=/root/backup.log
+SCRIPT_NAME=${0%.*}
+LOG_FILE="$(basename ${SCRIPT_NAME}).log"
+
 time=` date +"%Y%m%d %H%M%S"`
 
 
@@ -11,7 +14,7 @@ time=` date +"%Y%m%d %H%M%S"`
 
 
 echo "Begin backup at: $time" >> $FILE_LOG
-ndb_mgm -e "start backup" 2>&1 >> $FILE_LOG
+ndb_mgm --ndb-mgmd-host=${ndb_mgmd[1]},${ndb_mgmd[2]} -e "start backup" 2>&1 >> $FILE_LOG
 bck_status=$?
 
 if [ $bck_status -gt 0 ]
